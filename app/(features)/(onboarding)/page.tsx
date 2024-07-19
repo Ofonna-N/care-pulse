4089d5b21cae622f36ca1bb86345f6e7fe3a70cb
Ofonna-N <nwezeofonna@gmail.com>
2024-07-19 02:32:05 -0500

    restructure folder

---
+++ app/(features)/(onboarding)/page.tsx
@@ -0,0 +1,8 @@
+export default function OnBoardingPage() {
+  return (
+    <div className="h-screen flex">
+      <div className="h-screen flex-1 bg-red-600"></div>
+      <div className="h-screen flex-1 bg-red-800"></div>
+    </div>
+  );
+}
