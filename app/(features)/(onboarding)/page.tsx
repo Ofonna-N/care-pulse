d4cb068abb40b590bbf5a557bdddd47a59ebc4dc
Ofonna-N <nwezeofonna@gmail.com>
2024-07-26 22:58:53 -0500

    style page

--- app/(features)/(onboarding)/page.tsx
+++ app/(features)/(onboarding)/page.tsx
@@ -5,12 +5,16 @@ import Image from "next/image";
 export default function OnBoardingPage() {
   return (
     <div className="h-screen flex">
-      <div className="h-screen w-[50%] flex items-center">
-        <div className="w-[400px] mx-auto">
+      <div className="h-screen justify-center w-full lg:w-[50%] flex items-center">
+        <div>
           <OnboardingForm />
         </div>
       </div>
-      <div className="h-screen w-[50%] max-w-[50%] bg-red-800 relative flex-1">
+      <div
+        className="h-screen w-[50%] max-w-[50%] bg-red-800 relative 
+      hidden lg:flex justify-center items-center
+      "
+      >
         {/* <div className="bg-green-500 w-full h-full"> */}{" "}
         <Image
           src={onBoardingImage}
